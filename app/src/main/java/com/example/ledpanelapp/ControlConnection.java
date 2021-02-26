f84f26bcdceed7de1e5e83e96a51d1a44028b153
Enrico Schörnick <enrico.schoernick@mailbox.org>
2021-02-26 14:13:49 +0100

    added basic zeromq functionality

---
+++ app/src/main/java/com/example/ledpanelapp/ControlConnection.java
@@ -0,0 +1,32 @@
+package com.example.ledpanelapp;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+import org.zeromq.SocketType;
+
+public class ControlConnection {
+
+    ZContext context;
+    ZMQ.Socket socket;
+
+    String connectionStr;
+
+    public ControlConnection() {
+        context = new ZContext();
+        socket = context.createSocket(SocketType.REQ);
+    }
+
+    public boolean connect(String ip) {
+        connectionStr = "tcp://" + ip + ":5555";
+        return socket.connect(connectionStr);
+    }
+
+    public boolean disconnect() {
+        return socket.disconnect(connectionStr);
+    }
+
+    public byte[] send(String msgType, String payload) {
+        socket.send(msgType + " " + payload);
+        return socket.recv();
+    }
+}
