a6574ba62af0b460dba4f1fc1c621fb487bcc60b
Enrico Schörnick <enrico.schoernick@mailbox.org>
2021-02-26 16:39:07 +0100

    added basic connection functionality

--- app/src/main/java/com/example/ledpanelapp/MainActivity.java
+++ app/src/main/java/com/example/ledpanelapp/MainActivity.java
@@ -3,15 +3,58 @@ package com.example.ledpanelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainController controller;
+
+    // UI elements
+    private Button connectBtn;
+    private EditText ipText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         controller = new MainController();
+        findViews();
+    }
+
+    private void findViews() {
+        connectBtn = (Button)findViewById(R.id.button_connect);
+        ipText = (EditText)findViewById(R.id.editText_ip);
+    }
+
+    public void onClickConnect(View v) {
+        if (controller.isConnected()) {
+            //we are connected -> disconnect
+            controller.disconnect();
+            changeToDisconnected();
+        } else {
+            //we are disconnected -> connect
+            if (!Patterns.IP_ADDRESS.matcher(ipText.getText()).matches()) {
+                makeText(MainActivity.this, getResources().getString(R.string.falscheIp), LENGTH_SHORT).show();
+                return;
+            }
+            if (controller.connect(ipText.getText().toString())) {
+                changeToConnected();
+            }
+        }
+    }
+
+    public void changeToConnected() {
+        connectBtn.setText(getResources().getString(R.string.trennen));
+        ipText.setEnabled(false);
+    }
+
+    public void changeToDisconnected() {
+        connectBtn.setText(getResources().getString(R.string.verbinden));
+        ipText.setEnabled(true);
     }
 }
--- app/src/main/java/com/example/ledpanelapp/MainController.java
+++ app/src/main/java/com/example/ledpanelapp/MainController.java
@@ -1,17 +1,24 @@
 package com.example.ledpanelapp;
 
 public class MainController {
-    ControlConnection connection;
+    private ControlConnection connection;
+    private boolean isConnected = false;
 
     public MainController() {
         connection = new ControlConnection();
     }
 
     public boolean connect(String ip) {
+        isConnected = true;
         return connection.connect(ip);
     }
 
     public boolean disconnect() {
+        isConnected = false;
         return connection.disconnect();
+    }
+
+    public boolean isConnected() {
+        return isConnected;
     }
 }
